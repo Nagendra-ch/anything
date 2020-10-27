ca9e04d28a3e12904f558e2db20222348a11076a
Nagendra <nagas400@gmail.com>
2020-10-26 23:14:01 -0400

    fff

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "firstdemo" {
   key_name                    = "nag-NVirginia"
   ami                         = "ami-011b3ccf1bd6db744"
   instance_type               = "t2.micro"
-  vpc_security_group_ids = 	 ["sg-00319e8b7cfa57448"]
+  vpc_security_group_ids      =  ["sg-00319e8b7cfa57448"]
   #vpc_id = aws_vpc.default.id
   subnet_id                   = var.subnet
   associate_public_ip_address = true
@@ -21,7 +21,7 @@ resource "aws_instance" "firstdemo" {
       "sudo yum install ansible -y",
       "sudo yum install git -y",
       "git clone https://github.com/Nagendra-ch/test2.git /tmp/ans",
-      "sleep 60; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /tmp/ans/ngnixplay.yml"
+      "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /tmp/ans/ngnixplay.yml"
       ]
 
     connection {
