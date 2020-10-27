c42135fde60d366ba9211df5c2f4375190b0ae0f
Nagendra <nagas400@gmail.com>
2020-10-26 20:32:23 -0400

    kk

--- main.tf
+++ main.tf
@@ -1,24 +1,34 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
-data "aws_caller_identity" "default" {
-}
+resource "aws_instance" "firstdemo" {
+  key_name                    = "nag-NVirginia"
+  ami                         = "ami-098f16afa9edf40be"
+  instance_type               = "t2.micro"
+  #vpc_security_group_ids = 	 [aws_security_group.SSH-RDS-Connection.id]
+  #vpc_id = aws_vpc.default.id
+  subnet_id                   = var.subnet
+  associate_public_ip_address = true
 
-data "aws_region" "default" {
-}
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install python3-pip",
+      "sudo pip3 install ansible --user",
+      "sudo yum install git -y",
+      "git clone https://github.com/Nagendra-ch/test2.git /tmp/ans",
+      "sleep 60; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook /tmp/ans/ngnixplay.yml"
+      ]
 
-data "aws_partition" "default" {
-}
-
-data "aws_subnet" "default" {
-  id = var.subnet
-  filter {
-      name = "owner"
-      values = ["nagendrra"]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        =  self.public_ip
+      private_key = var.keyfile
+    }
   }
-}
-output "region" {
-  value = data.aws_region.default.name
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "subnet" {
   type        = string
   description = "VPC Subnet ID the instance is launched in"
-  default     = ""
+  default     = "subnet-0350e4f96a2ac1435"
+}
+
+
+variable "keyfile" {
+    type = string
 }
